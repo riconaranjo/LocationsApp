fd2560ecb6b7489ecfec76a91b17787b9ea38b30
Federico Naranjo <federico.naranjo@gmail.com>
2016-06-30 18:15:33 -0400

    Initial Commit

---
+++ Locations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Federico Naranjo Bellina on 30/6/16.
+//  Copyright © 2016 Rico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
